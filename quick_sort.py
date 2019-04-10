94078b0957ce4a010186558665d190109976cc6a
Jelle <osoraku@gmail.com>
2019-04-10 22:08:10 +0900

    rewrite exactly as in lecture
    I failed the assignment because my way wasn't exactly as in the lecture so I rewrote it

--- quick_sort.py
+++ quick_sort.py
@@ -3,51 +3,97 @@
 
 import unittest
 
+from math import floor
 
-def quick_sort(x: list) -> list:
-    """ Sort a list of numbers.
 
-    Example:
-      >>> quick_sort([3, 2, 1, 0, 4, 5])
-      [0, 1, 2, 3, 4, 5]
+work_done = 0
+
+
+def reset_work_done():
+
+    global work_done
+    work_done = 0
+
+    return
+
+
+def get_median_3_i(x: list) -> int:
+    """ Find index of the median of the leftmost, rightmost, and middle value of an array.
+
+    Example: median is in index 1 of input list
+        >>> get_median_3_i([3, 5, 6])
+        1
     """
 
-    # Choose pivot and move it to index 0 here
+    leftmost = x[0]
+    middle = x[floor((len(x)-0.01) / 2)]
+    rightmost = x[len(x)-1]
 
-    array_2_start_i = len(x)
+    if (
+      leftmost <= middle <= rightmost or
+      leftmost >= middle >= rightmost):
+        pivot_i = floor((len(x)-0.01) / 2)
+    elif (
+      middle <= leftmost <= rightmost or
+      middle >= leftmost >= rightmost):
+        pivot_i = 0
+    elif (
+      middle <= rightmost <= leftmost or
+      middle >= rightmost >= leftmost):
+        pivot_i = len(x)-1
+    else:
+        raise AssertionError("Impossible median case")
 
-    if len(x) == 2:
-        if x[0] > x[1]:
-            return [x[1], x[0]]
-        else:
-            return x
+    return pivot_i
+
+
+def quick_sort(x: list, pivot_choice: str = "leftmost") -> list:
+    """ Sort a list of numbers.
+
+    Please pass "leftmost", "rightmost", or "median 3" to the pivot_choice parameter.
+
+    Example:
+      >>> quick_sort([3, 8, 2, 5, 1, 4, 7, 6])
+      [1, 2, 3, 4, 5, 6, 7, 8]
+    """
+
+    global work_done
+    work_done += max(len(x)-1, 0)
+
+    # print("Current input:" + str(x) + " - Work done so far: " + str(work_done))
 
     if len(x) < 2:
         return x
 
-    i = 1
-    end_i = len(x)
-    while True:
-        if x[i] > x[0]:
-            x.append(x.pop(i))
-            i -= 1
-            end_i -= 1
-            array_2_start_i -= 1
-        i += 1
-        if i >= end_i:
-            break
-
-    if array_2_start_i > 1:
-        smaller_than_pivot = x[1:array_2_start_i]
+    # Pre-processing step:
+    # Choose pivot and move it to index 0 here
+    if pivot_choice == "leftmost":
+        pivot_i = 0
+    elif pivot_choice == "rightmost":
+        pivot_i = len(x)-1
+    elif pivot_choice == "median 3":
+        pivot_i = get_median_3_i(x)
     else:
-        smaller_than_pivot = []
+        raise ValueError("Please pass 'leftmost', 'rightmost' or 'median 3' to pivot_choice param.")
+    x[0], x[pivot_i] = x[pivot_i], x[0]
 
-    if array_2_start_i < len(x):
-        bigger_than_pivot = x[array_2_start_i:]
-    else:
-        bigger_than_pivot = []
+    last_below_pivot_i = 1
+    for unseen_i in range(1, len(x)):
+        if x[unseen_i] < x[0]:
+            x[last_below_pivot_i], x[unseen_i] = x[unseen_i], x[last_below_pivot_i]
+            last_below_pivot_i += 1
 
-    return quick_sort(smaller_than_pivot) + [x[0]] + quick_sort(bigger_than_pivot)
+    # Swap pivot to the middle
+    x[last_below_pivot_i-1], x[0] = x[0], x[last_below_pivot_i-1]
+
+    # Identify the chunks
+    smaller_chunk = x[:last_below_pivot_i-1]
+    assert isinstance(smaller_chunk, list)
+    pivot = x[last_below_pivot_i-1]
+    bigger_chunk = x[last_below_pivot_i:]
+    assert isinstance(bigger_chunk, list)
+
+    return quick_sort(smaller_chunk, pivot_choice) + [pivot] + quick_sort(bigger_chunk, pivot_choice)
 
 
 class TestQuickSort(unittest.TestCase):
@@ -56,12 +102,12 @@ class TestQuickSort(unittest.TestCase):
 
     def test_n_is_2(self):
         self.assertListEqual(
-            quick_sort([7, 2]),
+            quick_sort([7, 2], "median 3"),
             [2, 7])
 
     def test_n_is_2_already_sorted(self):
         self.assertListEqual(
-            quick_sort([1, 4]),
+            quick_sort([1, 4], "median 3"),
             [1, 4])
 
     def test_n_is_4(self):
@@ -71,7 +117,7 @@ class TestQuickSort(unittest.TestCase):
 
     def test_n_is_5(self):
         self.assertListEqual(
-            quick_sort([7, 2, 4, 5, 1]),
+            quick_sort([7, 2, 4, 5, 1], "median 3"),
             [1, 2, 4, 5, 7])
 
     def test_n_is_8(self):
@@ -86,13 +132,36 @@ class TestQuickSort(unittest.TestCase):
 
     def test_everything_above_pivot(self):
         self.assertListEqual(
-            quick_sort([0, 2, 4, 5, 1, 8, 3, 9, 10, 6]),
+            quick_sort([0, 2, 4, 5, 1, 8, 3, 9, 10, 6], "rightmost"),
             [0, 1, 2, 3, 4, 5, 6, 8, 9, 10])
 
     def test_everything_below_pivot(self):
         self.assertListEqual(
             quick_sort([100, 2, 4, 5, 1, 8, 3, 9, 10, 6]),
             [1, 2, 3, 4, 5, 6, 8, 9, 10, 100])
+
+
+class TestFindMedian3(unittest.TestCase):
+
+    def test_n_is_5(self):
+        self.assertEqual(
+            get_median_3_i([1, 2, 3, 4, 5]),
+            2)
+
+    def test_n_is_6(self):
+        self.assertEqual(
+            get_median_3_i([1, 2, 3, 4, 5, 20]),
+            2)
+
+    def test_n_is_6_right_most_median(self):
+        self.assertEqual(
+            get_median_3_i([1, 2, 20, 4, 5, 3]),
+            5)
+
+    def test_n_is_6_left_most_median(self):
+        self.assertEqual(
+            get_median_3_i([3, 2, 20, 4, 5, 1]),
+            0)
 
 
 if __name__ == "__main__":
